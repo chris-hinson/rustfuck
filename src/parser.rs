7bf948e5235c2e762ddc318fdc359bdaec31a75a
chris <chris@hinson.dev>
2022-02-07 22:19:14 -0500

    parsing is still broken, but I have defined a cfg for the lang so i plan on reimplemnting based on that

--- src/parser.rs
+++ src/parser.rs
@@ -1,112 +1,45 @@
 use crate::lexer::{Token, TokenKind};
+use std::cell::RefCell;
 use std::fmt::{Display, Formatter, Result as FmtResult};
 #[derive(Debug, Clone, PartialEq, Eq)]
-pub enum AstNode {
-    Move {
-        dir: bool,
-        amount: isize,
-        next: Option<Box<AstNode>>,
-    },
-    Change {
-        dir: bool,
-        amount: i128,
-        next: Option<Box<AstNode>>,
-    },
-    Output {
-        next: Option<Box<AstNode>>,
-    },
-    Input {
-        next: Option<Box<AstNode>>,
-    },
-    StartLoop {
-        next: Option<Box<AstNode>>,
-        jump: Option<Box<AstNode>>,
-    },
-    EndLoop {
-        next: Option<Box<AstNode>>,
-        jump: Option<Box<AstNode>>,
-    },
-    Err {},
+pub struct AstNode {
+    id: usize,
+    ntype: AstNodeKind,
+}
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub enum AstNodeKind {
+    Move { amount: isize },
+    Change { amount: i128 },
+    Output {},
+    Input {},
+    StartLoop { jump: Option<Box<AstNode>> },
+    EndLoop { jump: Option<Box<AstNode>> },
+    Eof {},
 }
 
 impl Display for AstNode {
     fn fmt(&self, f: &mut Formatter) -> FmtResult {
-        match self {
-            AstNode::Move { dir, amount, next } => write!(
-                f,
-                "move {} by {}",
-                (if *dir { "right" } else { "left" }),
-                amount
-            ),
-            AstNode::Change { dir, amount, next } => write!(
-                f,
-                "change dp by {}{}",
-                (if *dir { "" } else { "-" }),
-                amount
-            ),
-            AstNode::Output { .. } => {
+        match self.ntype {
+            AstNodeKind::Move { amount } => write!(f, "move {}", amount),
+            AstNodeKind::Change { amount } => {
+                write!(f, "change dp by {}", amount)
+            }
+            AstNodeKind::Output { .. } => {
                 write!(f, "output")
             }
-            AstNode::Input { .. } => {
+            AstNodeKind::Input { .. } => {
                 write!(f, "input")
             }
-            AstNode::StartLoop { .. } => {
+            AstNodeKind::StartLoop { .. } => {
                 write!(f, "start loop")
             }
-            AstNode::EndLoop { .. } => {
+            AstNodeKind::EndLoop { .. } => {
                 write!(f, "end loop")
             }
-            AstNode::Err {} => {
-                write!(f, "error node")
+            AstNodeKind::Eof {} => {
+                write!(f, "end of file")
             }
-        }
-    }
-}
-impl AstNode {
-    fn new(tok: Token) -> Self {
-        match tok.kind {
-            TokenKind::DpMove { dir } => match dir.as_str() {
-                "left" => AstNode::Move {
-                    dir: false,
-                    amount: 1,
-                    next: None,
-                },
-                "right" => AstNode::Move {
-                    dir: true,
-                    amount: 1,
-                    next: None,
-                },
-                _ => AstNode::Err {},
-            },
-            TokenKind::DataChange { dir } => match dir.as_str() {
-                "down" => AstNode::Change {
-                    dir: false,
-                    amount: 1,
-                    next: None,
-                },
-                "up" => AstNode::Move {
-                    dir: true,
-                    amount: 1,
-                    next: None,
-                },
-                _ => AstNode::Err {},
-            },
-            TokenKind::Io { dir } => match dir.as_str() {
-                "out" => AstNode::Output { next: None },
-                "in" => AstNode::Input { next: None },
-                _ => AstNode::Err {},
-            },
-            TokenKind::Loop { dir } => match dir.as_str() {
-                "left" => AstNode::StartLoop {
-                    next: None,
-                    jump: None,
-                },
-                "right" => AstNode::EndLoop {
-                    next: None,
-                    jump: None,
-                },
-                _ => AstNode::Err {},
-            },
         }
     }
 }
@@ -116,30 +49,113 @@ pub fn parse(mut tokens: Vec<Token>) -> Result<AstNode, String> {
         return Err("no more tokens".to_owned());
     }
     let token = tokens.remove(0);
-    let node = AstNode::new(token);
-    match node {
-        AstNode::Move {
-            dir: _,
-            amount: _,
-            ref next,
-        } => {
-            next = match parse(tokens) {
-                Ok(v) => &Some(Box::new(v)),
-                Err(_e) => &None,
-            };
-        }
+    let node = match token.kind {
+        TokenKind::DpMove { dir } => match dir.as_str() {
+            "left" => Ok(AstNode {
+                ntype: AstNodeKind::Move { amount: -1 },
+                next: match parse(tokens) {
+                    Ok(v) => Some(Box::new(RefCell::new(v))),
+                    Err(_e) => None,
+                },
+            }),
+            "right" => Ok(AstNode {
+                ntype: AstNodeKind::Move { amount: 1 },
+                next: match parse(tokens) {
+                    Ok(v) => Some(Box::new(RefCell::new(v))),
+                    Err(_e) => None,
+                },
+            }),
+            _ => Err("Dp move with bad direction".to_owned()),
+        },
+        TokenKind::DataChange { dir } => match dir.as_str() {
+            "down" => Ok(AstNode {
+                ntype: AstNodeKind::Change { amount: -1 },
+                next: match parse(tokens) {
+                    Ok(v) => Some(Box::new(RefCell::new(v))),
+                    Err(_e) => None,
+                },
+            }),
+            "up" => Ok(AstNode {
+                ntype: AstNodeKind::Change { amount: 1 },
+                next: match parse(tokens) {
+                    Ok(v) => Some(Box::new(RefCell::new(v))),
+                    Err(_e) => None,
+                },
+            }),
+            _ => Err("Dp move with bad direction".to_owned()),
+        },
+        TokenKind::Io { dir } => match dir.as_str() {
+            "out" => Ok(AstNode {
+                ntype: AstNodeKind::Output {},
+                next: match parse(tokens) {
+                    Ok(v) => Some(Box::new(RefCell::new(v))),
+                    Err(_e) => None,
+                },
+            }),
+            "in" => Ok(AstNode {
+                ntype: AstNodeKind::Input {},
+                next: match parse(tokens) {
+                    Ok(v) => Some(Box::new(RefCell::new(v))),
+                    Err(_e) => None,
+                },
+            }),
+            _ => Err("Dp move with bad direction".to_owned()),
+        },
+        TokenKind::Loop { dir } => match dir.as_str() {
+            "left" => Ok(AstNode {
+                ntype: AstNodeKind::StartLoop { jump: None },
+                next: match parse(tokens) {
+                    Ok(v) => Some(Box::new(RefCell::new(v))),
+                    Err(_e) => None,
+                },
+            }),
+            "right" => Ok(AstNode {
+                ntype: AstNodeKind::EndLoop { jump: None },
+                next: match parse(tokens) {
+                    Ok(v) => Some(Box::new(RefCell::new(v))),
+                    Err(_e) => None,
+                },
+            }),
+            _ => Err("Dp move with bad direction".to_owned()),
+        },
+    };
+
+    node
+}
+
+/*pub fn optimize(head: AstNode) -> AstNode {
+    let cur = &head;
+    match head.ntype {
+        AstNodeKind::Move { dir, amount } => match &cur.next {
+            Some(v) => {
+                if v.ntype
+                    == (AstNodeKind::Move {
+                        dir: true,
+                        amount: 1,
+                    })
+                {
+                    v.amount += 1;
+                }
+            }
+            None => {}
+        },
         _ => {}
     }
 
-    Ok(node)
-}
-
-/*fn optimize(mut nodes: Vec<AstNode>) -> Vec<AstNode> {
-    let mut new_nodes: Vec<AstNode> = Vec::new();
-
-    for (i, n) in nodes.iter_mut().enumerate() {
-        if nodes[i + 1].eq(n) {}
-    }
-
-    new_nodes
+    return head;
 }*/
+
+pub fn print_ast(mut head: AstNode) {
+    let mut cur = &mut head;
+    loop {
+        match cur.next {
+            Some(ref mut v) => {
+                println!("{}", v.get_mut());
+                cur = v.get_mut();
+            }
+            None => {
+                break;
+            }
+        }
+    }
+}
