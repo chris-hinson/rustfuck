95fe9a1e05608e070e4237da9346e69aab77ddab
chris <chris@hinson.dev>
2022-02-16 11:05:12 -0500

    git wtf stop adding back the target folder

--- target/debug/deps/rustfuck-43a64eb1b56e68b4.d
+++
@@ -1,9 +0,0 @@
-/home/chris/Documents/projects/rustfuck/target/debug/deps/rustfuck-43a64eb1b56e68b4: src/main.rs src/lexer.rs src/parser.rs src/runner.rs src/runner_naive.rs
-
-/home/chris/Documents/projects/rustfuck/target/debug/deps/rustfuck-43a64eb1b56e68b4.d: src/main.rs src/lexer.rs src/parser.rs src/runner.rs src/runner_naive.rs
-
-src/main.rs:
-src/lexer.rs:
-src/parser.rs:
-src/runner.rs:
-src/runner_naive.rs:
